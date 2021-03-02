aec7118521d76744fab329393101730364bd256f
thebadams <bpeteradams@gmail.com>
2021-03-02 14:40:00 -0500

    wrote a function to generate a random query

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,25 +7,45 @@ class query {
 }
 
 class option {
-    constructor(tAnswer, tIsCorrect){
+    constructor(tAnswer, tIsCorrect, tIsSelected){
         this.answer = tAnswer;
         this.isCorrect = tIsCorrect;
+        this.isSelected = tIsSelected
     }
 }
 
 //build queries and options
 
 //build options, query 1
-var q1Opt1 = new option("test answer", true);
-var q1Opt2 = new option("test answer 2", false);
-var q1Opt3 = new option("test answer 3", false);
-var q1Opt4 = new option("test answer 4", false);
+var q1Opt1 = new option("test answer", true, false);
+var q1Opt2 = new option("test answer 2", false, false);
+var q1Opt3 = new option("test answer 3", false, false);
+var q1Opt4 = new option("test answer 4", false, false);
 
 //build query 1 options array
 q1Options = [q1Opt1, q1Opt2, q1Opt3, q1Opt4];
 
 var q1 = new query("test question 1", q1Options);
 
+//build options, query 1
+var q2Opt1 = new option("test answer 5", true, false);
+var q2Opt2 = new option("test answer 6", false, false);
+var q2Opt3 = new option("test answer 7", false, false);
+var q2Opt4 = new option("test answer 8", false, false);
+
+//build query 1 options array
+q2Options = [q2Opt1, q2Opt2, q2Opt3, q2Opt4];
+
+var q2 = new query("test question 2", q2Options);
+
+//function to generate random question
+function generateQuery(){
+    let queryArray = [q1, q2];
+    let randomNum = Math.floor(Math.random()*queryArray.length)
+    let randomQuery = queryArray[randomNum];
+    return randomQuery;
+    
+};
 questionContent = document.querySelector("h2");
 questionContent.textContent = q1.question
 optionContent1 = document.querySelector("label[for=option1]");
